7d7e0dfaa9ae18b1756d77cf5f2e3cc9a5b5cb9f
Nathan Bolton <nathan@nathanbolton.ca>
2020-09-14 18:50:18 -0700

    Update Window.js
    switch to using BitmapCompatLayer to make window backs

--- js/rpg_core/Window.js
+++ js/rpg_core/Window.js
@@ -12,7 +12,7 @@ function Window() {
 Window.prototype = Object.create(PIXI.Container.prototype);
 Window.prototype.constructor = Window;
 
-Window.prototype.initialize = function() {
+Window.prototype.initialize = function () {
     PIXI.Container.call(this);
 
     this._isWindow = true;
@@ -85,10 +85,10 @@ Window.prototype.initialize = function() {
  * @type Bitmap
  */
 Object.defineProperty(Window.prototype, 'windowskin', {
-    get: function() {
+    get: function () {
         return this._windowskin;
     },
-    set: function(value) {
+    set: function (value) {
         if (this._windowskin !== value) {
             this._windowskin = value;
             this._windowskin.addLoadListener(this._onWindowskinLoad.bind(this));
@@ -104,10 +104,10 @@ Object.defineProperty(Window.prototype, 'windowskin', {
  * @type Bitmap
  */
 Object.defineProperty(Window.prototype, 'contents', {
-    get: function() {
+    get: function () {
         return this._windowContentsSprite.bitmap;
     },
-    set: function(value) {
+    set: function (value) {
         this._windowContentsSprite.bitmap = value;
     },
     configurable: true
@@ -120,10 +120,10 @@ Object.defineProperty(Window.prototype, 'contents', {
  * @type Number
  */
 Object.defineProperty(Window.prototype, 'width', {
-    get: function() {
+    get: function () {
         return this._width;
     },
-    set: function(value) {
+    set: function (value) {
         this._width = value;
         this._refreshAllParts();
     },
@@ -137,10 +137,10 @@ Object.defineProperty(Window.prototype, 'width', {
  * @type Number
  */
 Object.defineProperty(Window.prototype, 'height', {
-    get: function() {
+    get: function () {
         return this._height;
     },
-    set: function(value) {
+    set: function (value) {
         this._height = value;
         this._refreshAllParts();
     },
@@ -154,10 +154,10 @@ Object.defineProperty(Window.prototype, 'height', {
  * @type Number
  */
 Object.defineProperty(Window.prototype, 'padding', {
-    get: function() {
+    get: function () {
         return this._padding;
     },
-    set: function(value) {
+    set: function (value) {
         this._padding = value;
         this._refreshAllParts();
     },
@@ -171,10 +171,10 @@ Object.defineProperty(Window.prototype, 'padding', {
  * @type Number
  */
 Object.defineProperty(Window.prototype, 'margin', {
-    get: function() {
+    get: function () {
         return this._margin;
     },
-    set: function(value) {
+    set: function (value) {
         this._margin = value;
         this._refreshAllParts();
     },
@@ -188,10 +188,10 @@ Object.defineProperty(Window.prototype, 'margin', {
  * @type Number
  */
 Object.defineProperty(Window.prototype, 'opacity', {
-    get: function() {
+    get: function () {
         return this._windowSpriteContainer.alpha * 255;
     },
-    set: function(value) {
+    set: function (value) {
         this._windowSpriteContainer.alpha = value.clamp(0, 255) / 255;
     },
     configurable: true
@@ -204,10 +204,10 @@ Object.defineProperty(Window.prototype, 'opacity', {
  * @type Number
  */
 Object.defineProperty(Window.prototype, 'backOpacity', {
-    get: function() {
+    get: function () {
         return this._windowBackSprite.alpha * 255;
     },
-    set: function(value) {
+    set: function (value) {
         this._windowBackSprite.alpha = value.clamp(0, 255) / 255;
     },
     configurable: true
@@ -220,10 +220,10 @@ Object.defineProperty(Window.prototype, 'backOpacity', {
  * @type Number
  */
 Object.defineProperty(Window.prototype, 'contentsOpacity', {
-    get: function() {
+    get: function () {
         return this._windowContentsSprite.alpha * 255;
     },
-    set: function(value) {
+    set: function (value) {
         this._windowContentsSprite.alpha = value.clamp(0, 255) / 255;
     },
     configurable: true
@@ -236,10 +236,10 @@ Object.defineProperty(Window.prototype, 'contentsOpacity', {
  * @type Number
  */
 Object.defineProperty(Window.prototype, 'openness', {
-    get: function() {
+    get: function () {
         return this._openness;
     },
-    set: function(value) {
+    set: function (value) {
         if (this._openness !== value) {
             this._openness = value.clamp(0, 255);
             this._windowSpriteContainer.scale.y = this._openness / 255;
@@ -254,11 +254,11 @@ Object.defineProperty(Window.prototype, 'openness', {
  *
  * @method update
  */
-Window.prototype.update = function() {
+Window.prototype.update = function () {
     if (this.active) {
         this._animationCount++;
     }
-    this.children.forEach(function(child) {
+    this.children.forEach(function (child) {
         if (child.update) {
             child.update();
         }
@@ -274,7 +274,7 @@ Window.prototype.update = function() {
  * @param {Number} width The width of the window
  * @param {Number} height The height of the window
  */
-Window.prototype.move = function(x, y, width, height) {
+Window.prototype.move = function (x, y, width, height) {
     this.x = x || 0;
     this.y = y || 0;
     if (this._width !== width || this._height !== height) {
@@ -289,7 +289,7 @@ Window.prototype.move = function(x, y, width, height) {
  *
  * @method isOpen
  */
-Window.prototype.isOpen = function() {
+Window.prototype.isOpen = function () {
     return this._openness >= 255;
 };
 
@@ -298,7 +298,7 @@ Window.prototype.isOpen = function() {
  *
  * @method isClosed
  */
-Window.prototype.isClosed = function() {
+Window.prototype.isClosed = function () {
     return this._openness <= 0;
 };
 
@@ -311,7 +311,7 @@ Window.prototype.isClosed = function() {
  * @param {Number} width The width of the cursor
  * @param {Number} height The height of the cursor
  */
-Window.prototype.setCursorRect = function(x, y, width, height) {
+Window.prototype.setCursorRect = function (x, y, width, height) {
     var cx = Math.floor(x || 0);
     var cy = Math.floor(y || 0);
     var cw = Math.floor(width || 0);
@@ -334,7 +334,7 @@ Window.prototype.setCursorRect = function(x, y, width, height) {
  * @param {Number} g The green value in the range (-255, 255)
  * @param {Number} b The blue value in the range (-255, 255)
  */
-Window.prototype.setTone = function(r, g, b) {
+Window.prototype.setTone = function (r, g, b) {
     var tone = this._colorTone;
     if (r !== tone[0] || g !== tone[1] || b !== tone[2]) {
         this._colorTone = [r, g, b];
@@ -349,7 +349,7 @@ Window.prototype.setTone = function(r, g, b) {
  * @param {Object} child The child to add
  * @return {Object} The child that was added
  */
-Window.prototype.addChildToBack = function(child) {
+Window.prototype.addChildToBack = function (child) {
     var containerIndex = this.children.indexOf(this._windowSpriteContainer);
     return this.addChildAt(child, containerIndex + 1);
 };
@@ -358,7 +358,7 @@ Window.prototype.addChildToBack = function(child) {
  * @method updateTransform
  * @private
  */
-Window.prototype.updateTransform = function() {
+Window.prototype.updateTransform = function () {
     this._updateCursor();
     this._updateArrows();
     this._updatePauseSign();
@@ -370,11 +370,11 @@ Window.prototype.updateTransform = function() {
  * @method _createAllParts
  * @private
  */
-Window.prototype._createAllParts = function() {
+Window.prototype._createAllParts = function () {
     this._windowSpriteContainer = new PIXI.Container();
-    this._windowBackSprite = new Sprite();
-    this._windowCursorSprite = new Sprite();
-    this._windowFrameSprite = new Sprite();
+    this._windowBackSprite = new PIXI.Container();
+    this._windowCursorSprite = new PIXI.Container();
+    this._windowFrameSprite = new PIXI.Container();
     this._windowContentsSprite = new Sprite();
     this._downArrowSprite = new Sprite();
     this._upArrowSprite = new Sprite();
@@ -395,7 +395,7 @@ Window.prototype._createAllParts = function() {
  * @method _onWindowskinLoad
  * @private
  */
-Window.prototype._onWindowskinLoad = function() {
+Window.prototype._onWindowskinLoad = function () {
     this._refreshAllParts();
 };
 
@@ -403,7 +403,7 @@ Window.prototype._onWindowskinLoad = function() {
  * @method _refreshAllParts
  * @private
  */
-Window.prototype._refreshAllParts = function() {
+Window.prototype._refreshAllParts = function () {
     this._refreshBack();
     this._refreshFrame();
     this._refreshCursor();
@@ -416,17 +416,19 @@ Window.prototype._refreshAllParts = function() {
  * @method _refreshBack
  * @private
  */
-Window.prototype._refreshBack = function() {
+Window.prototype._refreshBack = function () {
     var m = this._margin;
     var w = this._width - m * 2;
     var h = this._height - m * 2;
-    var bitmap = new Bitmap(w, h);
+    var bitmap = new BitmapCompatLayer(w, h);
 
-    this._windowBackSprite.bitmap = bitmap;
-    this._windowBackSprite.setFrame(0, 0, w, h);
-    this._windowBackSprite.move(m, m);
+    this._windowBackSprite.width = w;
+    this._windowBackSprite.height = h;
+    this._windowBackSprite.x = m;
+    this._windowBackSprite.y = m;
+    //this._windowBackSprite.setFrame(0, 0, w, h);
 
-    if (w > 0 && h > 0 && this._windowskin) {
+    if (w > 0 && h > 0 && this._windowskin && !this._windowBackSprite._hasChildren) {
         var p = 96;
         bitmap.blt(this._windowskin, 0, 0, p, p, 0, 0, w, h);
         for (var y = 0; y < h; y += p) {
@@ -434,8 +436,11 @@ Window.prototype._refreshBack = function() {
                 bitmap.blt(this._windowskin, 0, p, p, p, x, y, p, p);
             }
         }
-        var tone = this._colorTone;
-        bitmap.adjustTone(tone[0], tone[1], tone[2]);
+        // No longer has support for adjusting tone
+        //var tone = this._colorTone;
+        //bitmap.adjustTone(tone[0], tone[1], tone[2]);
+        this._windowBackSprite.addChild(bitmap);
+        this._windowBackSprite._hasChildren = true;
     }
 };
 
@@ -443,27 +448,30 @@ Window.prototype._refreshBack = function() {
  * @method _refreshFrame
  * @private
  */
-Window.prototype._refreshFrame = function() {
+Window.prototype._refreshFrame = function () {
     var w = this._width;
     var h = this._height;
     var m = 24;
-    var bitmap = new Bitmap(w, h);
+    var bitmap = new BitmapCompatLayer(w, h);
 
-    this._windowFrameSprite.bitmap = bitmap;
-    this._windowFrameSprite.setFrame(0, 0, w, h);
+    this._windowFrameSprite.width = w;
+    this._windowFrameSprite.height = h;
+    //this._windowFrameSprite.setFrame(0, 0, w, h);
 
-    if (w > 0 && h > 0 && this._windowskin) {
+    if (w > 0 && h > 0 && this._windowskin && !this._windowFrameSprite._hasChildren) {
         var skin = this._windowskin;
         var p = 96;
         var q = 96;
-        bitmap.blt(skin, p+m, 0+0, p-m*2, m, m, 0, w-m*2, m);
-        bitmap.blt(skin, p+m, 0+q-m, p-m*2, m, m, h-m, w-m*2, m);
-        bitmap.blt(skin, p+0, 0+m, m, p-m*2, 0, m, m, h-m*2);
-        bitmap.blt(skin, p+q-m, 0+m, m, p-m*2, w-m, m, m, h-m*2);
-        bitmap.blt(skin, p+0, 0+0, m, m, 0, 0, m, m);
-        bitmap.blt(skin, p+q-m, 0+0, m, m, w-m, 0, m, m);
-        bitmap.blt(skin, p+0, 0+q-m, m, m, 0, h-m, m, m);
-        bitmap.blt(skin, p+q-m, 0+q-m, m, m, w-m, h-m, m, m);
+        bitmap.blt(skin, p + m, 0 + 0, p - m * 2, m, m, 0, w - m * 2, m);
+        bitmap.blt(skin, p + m, 0 + q - m, p - m * 2, m, m, h - m, w - m * 2, m);
+        bitmap.blt(skin, p + 0, 0 + m, m, p - m * 2, 0, m, m, h - m * 2);
+        bitmap.blt(skin, p + q - m, 0 + m, m, p - m * 2, w - m, m, m, h - m * 2);
+        bitmap.blt(skin, p + 0, 0 + 0, m, m, 0, 0, m, m);
+        bitmap.blt(skin, p + q - m, 0 + 0, m, m, w - m, 0, m, m);
+        bitmap.blt(skin, p + 0, 0 + q - m, m, m, 0, h - m, m, m);
+        bitmap.blt(skin, p + q - m, 0 + q - m, m, m, w - m, h - m, m, m);
+        this._windowFrameSprite.addChild(bitmap);
+        this._windowFrameSprite._hasChildren = true;
     }
 };
 
@@ -471,7 +479,7 @@ Window.prototype._refreshFrame = function() {
  * @method _refreshCursor
  * @private
  */
-Window.prototype._refreshCursor = function() {
+Window.prototype._refreshCursor = function () {
     var pad = this._padding;
     var x = this._cursorRect.x + pad - this.origin.x;
     var y = this._cursorRect.y + pad - this.origin.y;
@@ -484,25 +492,29 @@ Window.prototype._refreshCursor = function() {
     var oy = y - y2;
     var w2 = Math.min(w, this._width - pad - x2);
     var h2 = Math.min(h, this._height - pad - y2);
-    var bitmap = new Bitmap(w2, h2);
+    var bitmap = new BitmapCompatLayer(w2, h2);
 
-    this._windowCursorSprite.bitmap = bitmap;
-    this._windowCursorSprite.setFrame(0, 0, w2, h2);
-    this._windowCursorSprite.move(x2, y2);
+    this._windowCursorSprite.x = x;
+    this._windowCursorSprite.y = y;
+    this._windowCursorSprite.width = w;
+    this._windowCursorSprite.height = h;
+    //this._windowCursorSprite.setFrame(0, 0, w2, h2);
 
-    if (w > 0 && h > 0 && this._windowskin) {
+    if (w > 0 && h > 0 && this._windowskin && !this._windowCursorSprite._hasChildren) {
         var skin = this._windowskin;
         var p = 96;
         var q = 48;
-        bitmap.blt(skin, p+m, p+m, q-m*2, q-m*2, ox+m, oy+m, w-m*2, h-m*2);
-        bitmap.blt(skin, p+m, p+0, q-m*2, m, ox+m, oy+0, w-m*2, m);
-        bitmap.blt(skin, p+m, p+q-m, q-m*2, m, ox+m, oy+h-m, w-m*2, m);
-        bitmap.blt(skin, p+0, p+m, m, q-m*2, ox+0, oy+m, m, h-m*2);
-        bitmap.blt(skin, p+q-m, p+m, m, q-m*2, ox+w-m, oy+m, m, h-m*2);
-        bitmap.blt(skin, p+0, p+0, m, m, ox+0, oy+0, m, m);
-        bitmap.blt(skin, p+q-m, p+0, m, m, ox+w-m, oy+0, m, m);
-        bitmap.blt(skin, p+0, p+q-m, m, m, ox+0, oy+h-m, m, m);
-        bitmap.blt(skin, p+q-m, p+q-m, m, m, ox+w-m, oy+h-m, m, m);
+        bitmap.blt(skin, p + m, p + m, q - m * 2, q - m * 2, ox + m, oy + m, w - m * 2, h - m * 2);
+        bitmap.blt(skin, p + m, p + 0, q - m * 2, m, ox + m, oy + 0, w - m * 2, m);
+        bitmap.blt(skin, p + m, p + q - m, q - m * 2, m, ox + m, oy + h - m, w - m * 2, m);
+        bitmap.blt(skin, p + 0, p + m, m, q - m * 2, ox + 0, oy + m, m, h - m * 2);
+        bitmap.blt(skin, p + q - m, p + m, m, q - m * 2, ox + w - m, oy + m, m, h - m * 2);
+        bitmap.blt(skin, p + 0, p + 0, m, m, ox + 0, oy + 0, m, m);
+        bitmap.blt(skin, p + q - m, p + 0, m, m, ox + w - m, oy + 0, m, m);
+        bitmap.blt(skin, p + 0, p + q - m, m, m, ox + 0, oy + h - m, m, m);
+        bitmap.blt(skin, p + q - m, p + q - m, m, m, ox + w - m, oy + h - m, m, m);
+        this._windowCursorSprite.addChild(bitmap);
+        this._windowCursorSprite._hasChildren = true;
     }
 };
 
@@ -510,7 +522,7 @@ Window.prototype._refreshCursor = function() {
  * @method _refreshContents
  * @private
  */
-Window.prototype._refreshContents = function() {
+Window.prototype._refreshContents = function () {
     this._windowContentsSprite.move(this.padding, this.padding);
 };
 
@@ -518,30 +530,30 @@ Window.prototype._refreshContents = function() {
  * @method _refreshArrows
  * @private
  */
-Window.prototype._refreshArrows = function() {
+Window.prototype._refreshArrows = function () {
     var w = this._width;
     var h = this._height;
     var p = 24;
-    var q = p/2;
-    var sx = 96+p;
-    var sy = 0+p;
+    var q = p / 2;
+    var sx = 96 + p;
+    var sy = 0 + p;
     this._downArrowSprite.bitmap = this._windowskin;
     this._downArrowSprite.anchor.x = 0.5;
     this._downArrowSprite.anchor.y = 0.5;
-    this._downArrowSprite.setFrame(sx+q, sy+q+p, p, q);
-    this._downArrowSprite.move(w/2, h-q);
+    this._downArrowSprite.setFrame(sx + q, sy + q + p, p, q);
+    this._downArrowSprite.move(w / 2, h - q);
     this._upArrowSprite.bitmap = this._windowskin;
     this._upArrowSprite.anchor.x = 0.5;
     this._upArrowSprite.anchor.y = 0.5;
-    this._upArrowSprite.setFrame(sx+q, sy, p, q);
-    this._upArrowSprite.move(w/2, q);
+    this._upArrowSprite.setFrame(sx + q, sy, p, q);
+    this._upArrowSprite.move(w / 2, q);
 };
 
 /**
  * @method _refreshPauseSign
  * @private
  */
-Window.prototype._refreshPauseSign = function() {
+Window.prototype._refreshPauseSign = function () {
     var sx = 144;
     var sy = 96;
     var p = 24;
@@ -557,7 +569,7 @@ Window.prototype._refreshPauseSign = function() {
  * @method _updateCursor
  * @private
  */
-Window.prototype._updateCursor = function() {
+Window.prototype._updateCursor = function () {
     var blinkCount = this._animationCount % 40;
     var cursorOpacity = this.contentsOpacity;
     if (this.active) {
@@ -575,7 +587,7 @@ Window.prototype._updateCursor = function() {
  * @method _updateContents
  * @private
  */
-Window.prototype._updateContents = function() {
+Window.prototype._updateContents = function () {
     var w = this._width - this._padding * 2;
     var h = this._height - this._padding * 2;
     if (w > 0 && h > 0) {
@@ -591,7 +603,7 @@ Window.prototype._updateContents = function() {
  * @method _updateArrows
  * @private
  */
-Window.prototype._updateArrows = function() {
+Window.prototype._updateArrows = function () {
     this._downArrowSprite.visible = this.isOpen() && this.downArrowVisible;
     this._upArrowSprite.visible = this.isOpen() && this.upArrowVisible;
 };
@@ -600,7 +612,7 @@ Window.prototype._updateArrows = function() {
  * @method _updatePauseSign
  * @private
  */
-Window.prototype._updatePauseSign = function() {
+Window.prototype._updatePauseSign = function () {
     var sprite = this._windowPauseSignSprite;
     var x = Math.floor(this._animationCount / 16) % 2;
     var y = Math.floor(this._animationCount / 16 / 2) % 2;
@@ -612,7 +624,7 @@ Window.prototype._updatePauseSign = function() {
     } else if (sprite.alpha < 1) {
         sprite.alpha = Math.min(sprite.alpha + 0.1, 1);
     }
-    sprite.setFrame(sx+x*p, sy+y*p, p, p);
+    sprite.setFrame(sx + x * p, sy + y * p, p, p);
     sprite.visible = this.isOpen();
 };
 
