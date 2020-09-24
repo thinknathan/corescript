88183443cd96c9490958b5c53b8ad6de85c3df1e
Nathan Bolton <nathan@nathanbolton.ca>
2020-09-23 18:21:00 -0700

    switch back to using PIXI.Text

--- js/rpg_core/BitmapPIXI.js
+++ js/rpg_core/BitmapPIXI.js
@@ -12,12 +12,8 @@ function BitmapPIXI() {
 BitmapPIXI.prototype = Object.create(PIXI.Container.prototype);
 BitmapPIXI.prototype.constructor = BitmapPIXI;
 
-BitmapPIXI.prototype.initialize = function (width, height, createCanvas) {
+BitmapPIXI.prototype.initialize = function (width, height) {
     PIXI.Container.call(this);
-    if (createCanvas) {
-        this._createCanvas(width, height);
-    }
-
     if (width) this.width = width;
     if (height) this.height = height;
     this._paintOpacity = 255;
@@ -31,75 +27,6 @@ BitmapPIXI.prototype.initialize = function (width, height, createCanvas) {
     this.textColor = '#ffffff';
     this.outlineColor = 'rgba(0, 0, 0, 0.5)';
     this.outlineWidth = 4;
-
-    this._spriteContainer = new PIXI.Container();
-    this._spriteContainer.width = width;
-    this._spriteContainer.height = height;
-    this.addChild(this._spriteContainer);
-
-    this._canvasContainer = new PIXI.Container();
-    this._canvasContainer._sprite = null;
-    this._canvasContainer.width = width;
-    this._canvasContainer.height = height;
-    this.addChild(this._canvasContainer);
-};
-
-BitmapPIXI.prototype._renderCanvas_PIXI = PIXI.Container.prototype._renderCanvas;
-BitmapPIXI.prototype._render_PIXI = PIXI.Container.prototype._render;
-
-BitmapPIXI.prototype._renderCanvas = function (renderer) {
-    this.checkDirty();
-    this._renderCanvas_PIXI(renderer);
-};
-
-BitmapPIXI.prototype._render = function (renderer) {
-    this.checkDirty();
-    this._render_PIXI(renderer);
-};
-
-BitmapPIXI.prototype.checkDirty = function () {
-    if (this._dirty) {
-        this._baseTexture.update();
-        let container = this._canvasContainer;
-        let baseTexture = this._baseTexture;
-        setTimeout(function () {
-            baseTexture.update();
-            let texture = new PIXI.Texture(baseTexture);
-            texture.CREATED_BY = this;
-            texture.CREATED_AT = Date.now();
-            if (container._sprite) {
-                container._sprite.texture.destroy();
-                container._sprite.texture = texture;
-            } else {
-                container._sprite = new PIXI.Sprite(texture);
-                container.addChild(container._sprite);
-            }
-
-        }, 0);
-        this._dirty = false;
-    }
-};
-
-BitmapPIXI.prototype._setDirty = function () {
-    this._dirty = true;
-};
-
-BitmapPIXI.prototype._createCanvas = function (width, height) {
-    Bitmap.prototype._createCanvas.call(this, width, height);
-};
-
-BitmapPIXI.prototype._createBaseTexture = function (source) {
-    Bitmap.prototype._createBaseTexture.call(this, source);
-    this.__baseTexture.CREATED_BY = this;
-    this.__baseTexture.CREATED_AT = Date.now();
-};
-
-BitmapPIXI.prototype.resize = function (width, height) {
-    Bitmap.prototype.resize.call(this, width, height);
-    this._canvasContainer.width = width;
-    this._canvasContainer.height = height;
-    this._spriteContainer.width = width;
-    this._spriteContainer.height = height;
 };
 
 Object.defineProperty(BitmapPIXI.prototype, 'paintOpacity', {
@@ -109,51 +36,7 @@ Object.defineProperty(BitmapPIXI.prototype, 'paintOpacity', {
     set: function (value) {
         if (this._paintOpacity !== value) {
             this._paintOpacity = value;
-            this.alpha = this._paintOpacity / 255;
-            this._context.globalAlpha = this._paintOpacity / 255;
         }
-    },
-    configurable: true
-});
-
-Object.defineProperties(BitmapPIXI.prototype, {
-    _canvas: {
-        get: function () {
-            if (!this.__canvas) this._createCanvas();
-            return this.__canvas;
-        }
-    },
-    _context: {
-        get: function () {
-            if (!this.__context) this._createCanvas();
-            return this.__context;
-        }
-    },
-    _baseTexture: {
-        get: function () {
-            if (!this.__baseTexture) this._createBaseTexture(this._image || this.__canvas);
-            return this.__baseTexture;
-        }
-    }
-});
-
-Object.defineProperty(BitmapPIXI.prototype, 'baseTexture', {
-    get: function () {
-        return this._baseTexture;
-    },
-    configurable: true
-});
-
-Object.defineProperty(BitmapPIXI.prototype, 'canvas', {
-    get: function () {
-        return this._canvas;
-    },
-    configurable: true
-});
-
-Object.defineProperty(BitmapPIXI.prototype, 'context', {
-    get: function () {
-        return this._context;
     },
     configurable: true
 });
@@ -163,22 +46,20 @@ Object.defineProperty(BitmapPIXI.prototype, 'context', {
 
 
 BitmapPIXI.prototype.clear = function () {
-    for (let i = this._spriteContainer.children.length - 1; i >= 0; i--) {
-        this._spriteContainer.children[i].destroy({
+    for (let i = this.children.length - 1; i >= 0; i--) {
+        this.children[i].destroy({
             children: true,
             texture: true,
         });
-        this._spriteContainer.removeChild(this.children[i]);
+        this.removeChild(this.children[i]);
     }
-
-    Bitmap.prototype.clear.call(this);
 };
 
 BitmapPIXI.prototype.clearRect = function (x, y, width, height) {
     let self = this;
     let toRemove = [];
 
-    this._spriteContainer.children.forEach(function (child) {
+    this.children.forEach(function (child) {
         if (child &&
             (child.x >= x && child.x < x + width) &&
             (child.y >= y && child.y < y + height)
@@ -192,10 +73,8 @@ BitmapPIXI.prototype.clearRect = function (x, y, width, height) {
             children: true,
             texture: true,
         });
-        self._spriteContainer.removeChild(child);
+        self.removeChild(child);
     });
-
-    Bitmap.prototype.clearRect.call(this, x, y, width, height);
 };
 
 
@@ -204,29 +83,6 @@ BitmapPIXI.prototype.clearRect = function (x, y, width, height) {
 
 BitmapPIXI.prototype.drawText = function (text, x, y, maxWidth, lineHeight, align) {
     if (text === undefined) return;
-    let tx = x;
-    let ty = y + lineHeight - Math.round((lineHeight - this.fontSize * 0.7) / 2);
-    let context = this._context;
-    let alpha = context.globalAlpha;
-    maxWidth = maxWidth || 0xffffffff;
-    if (align === 'center') {
-        tx += maxWidth / 2;
-    }
-    if (align === 'right') {
-        tx += maxWidth;
-    }
-    context.save();
-    context.font = this._makeFontNameText();
-    context.textAlign = align;
-    context.textBaseline = 'alphabetic';
-    context.globalAlpha = 1;
-    this._drawTextOutline(text, tx, ty, maxWidth);
-    context.globalAlpha = alpha;
-    this._drawTextBody(text, tx, ty, maxWidth);
-    context.restore();
-    this._setDirty();
-
-    /*
     let style = {
         fontFamily: this.fontFace,
         fontSize: this.fontSize,
@@ -252,13 +108,11 @@ BitmapPIXI.prototype.drawText = function (text, x, y, maxWidth, lineHeight, alig
         pixiText.x = x + maxWidth;
     }
 
+    pixiText.alpha = this._paintOpacity / 255;
     if (pixiText) this.addChild(pixiText);
-    */
 };
 
 BitmapPIXI.prototype.measureTextWidth = function (text) {
-    return Bitmap.prototype.measureTextWidth.call(this, text);
-    /*
     let style = {
         fontFamily: this.fontFace,
         fontSize: this.fontSize,
@@ -268,19 +122,6 @@ BitmapPIXI.prototype.measureTextWidth = function (text) {
     let width = pixiText.width;
     pixiText.destroy(true);
     return width;
-    */
-};
-
-BitmapPIXI.prototype._makeFontNameText = function () {
-    return Bitmap.prototype._makeFontNameText.call(this);
-};
-
-BitmapPIXI.prototype._drawTextOutline = function (text, tx, ty, maxWidth) {
-    Bitmap.prototype._drawTextOutline.call(this, text, tx, ty, maxWidth);
-};
-
-BitmapPIXI.prototype._drawTextBody = function (text, tx, ty, maxWidth) {
-    Bitmap.prototype._drawTextBody.call(this, text, tx, ty, maxWidth);
 };
 
 
@@ -325,7 +166,8 @@ BitmapPIXI.prototype.blt = function (source, sx, sy, sw, sh, dx, dy, dw, dh) {
             sprite.y = dy;
             sprite.width = dw;
             sprite.height = dh;
-            this._spriteContainer.addChild(sprite);
+            sprite.alpha = this._paintOpacity / 255;
+            this.addChild(sprite);
         }
     }
 };
@@ -341,12 +183,12 @@ BitmapPIXI.prototype.fillRect = function (x, y, width, height, color) {
         height,
     );
     rectangle.endFill();
-    if (rectangle) this._spriteContainer.addChild(rectangle);
+    rectangle.alpha = this._paintOpacity / 255;
+    if (rectangle) this.addChild(rectangle);
 };
 
 BitmapPIXI.prototype.gradientFillRect = function (x, y, width, height, color1, color2, vertical) {
-    const rectangle = this.fillRect(x, y, width, height, color1);
-    if (rectangle) this._spriteContainer.addChild(rectangle);
+    this.fillRect(x, y, width, height, color1);
 };
 
 BitmapPIXI.prototype.drawCircle = function (x, y, radius, color) {
@@ -359,5 +201,6 @@ BitmapPIXI.prototype.drawCircle = function (x, y, radius, color) {
         radius,
     );
     circle.endFill();
-    if (circle) this._spriteContainer.addChild(circle);
+    circle.alpha = this._paintOpacity / 255;
+    if (circle) this.addChild(circle);
 };
