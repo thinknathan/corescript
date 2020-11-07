930e1cf55664832d3243d7a6508be8c7a7e17d3f
Nathan Bolton <nathan@nathanbolton.ca>
2020-11-07 09:23:55 -0800

    Update main.js
    try turning on mipmaps

--- js/main.js
+++ js/main.js
@@ -272,6 +272,7 @@ PluginManager.onLoad = function(e) {
         this.removeCounter();
         PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
         PIXI.settings.ROUND_PIXELS = true;
+        PIXI.settings.MIPMAP_TEXTURES = PIXI.MIPMAP_MODES.ON;
         PIXI.settings.GC_MODE = PIXI.GC_MODES.AUTO;
         PIXI.settings.GC_MAX_IDLE = 150;
         SceneManager.run(Scene_Boot);
