76e5d891219bc1bcf23075a17c705b315994d314
Nathan Bolton <nathan@nathanbolton.ca>
2021-08-05 21:49:50 -0700

    Update headers

--- js/rpg_managers/_header.js
+++ js/rpg_managers/_header.js
@@ -1,3 +1,7 @@
 //=============================================================================
 // rpg_managers.js v1.6.1 (community-1.4)
+// Copyright (c) 2015 KADOKAWA CORPORATION./YOJI OJIMA
+// Released under the MIT License.
+// @license https://github.com/thinknathan/corescript/blob/master/LICENSE
+// @credit https://github.com/thinknathan/corescript/blob/master/CONTRIBUTORS.md
 //=============================================================================
