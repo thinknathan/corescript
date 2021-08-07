2f1e6f9ead6d925ae802178c958baa2ae98ce5a5
Nathan Bolton <nathan@nathanbolton.ca>
2021-08-07 09:04:22 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
 (function () {
 	PluginManager.setup($plugins);
 
-	window.addEventListener('load', (event) => {
+	const init = function () {
 		document.body.classList.remove('is-loading');
 		PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
 		PIXI.settings.ROUND_PIXELS = true;
-		PIXI.settings.MIPMAP_TEXTURES = PIXI.MIPMAP_MODES.ON;
+		PIXI.settings.MIPMAP_TEXTURES = PIXI.MIPMAP_MODES.OFF;
 		PIXI.settings.GC_MODE = PIXI.GC_MODES.MANUAL;
 		SceneManager.run(Scene_Boot);
-	});
+	};
+
+	if (document.readyState === 'complete') {
+		init();
+	} else {
+		window.addEventListener('load', () => {
+			init();
+		});
+	}
 })();
