10ae6ae5cf33637eacace559c548216fc7dcf340
Nathan Bolton <nathan@nathanbolton.ca>
2021-08-17 22:35:02 -0700

    Update main.js
    re-enable auto GC and enable autoDensity when relevant

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 (function () {
 	PluginManager.setup($plugins);
 
-	const init = function () {
+	const init = () => {
 		document.body.classList.remove('is-loading');
 		PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
 		PIXI.settings.ROUND_PIXELS = true;
+		PIXI.settings.GC_MAX_IDLE = 600;
 		PIXI.settings.MIPMAP_TEXTURES = PIXI.MIPMAP_MODES.OFF;
-		PIXI.settings.GC_MODE = PIXI.GC_MODES.MANUAL;
+		if (window.devicePixelRatio > 1) {
+			PIXI.settings.RENDER_OPTIONS.autoDensity = true;
+			PIXI.settings.RESOLUTION = window.devicePixelRatio;
+		}
 		SceneManager.run(Scene_Boot);
 	};
 
