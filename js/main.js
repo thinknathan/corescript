baed97410b6b190166acae36e6871bff5c6bd291
Nathan Bolton <nathan@nathanbolton.ca>
2021-08-20 11:51:21 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,16 +7,9 @@
 		PIXI.settings.ROUND_PIXELS = true;
 		PIXI.settings.GC_MAX_IDLE = 600;
 		PIXI.settings.MIPMAP_TEXTURES = PIXI.MIPMAP_MODES.OFF;
-		if (window.devicePixelRatio > 1) {
-			PIXI.settings.RENDER_OPTIONS.autoDensity = true;
-			PIXI.settings.RESOLUTION = window.devicePixelRatio;
-		}
+		PIXI.settings.RESOLUTION = window.devicePixelRatio;
 		if (Utils.isMobileSafari()) {
 			PIXI.settings.PRECISION_FRAGMENT = PIXI.PRECISION.HIGH;
-			PIXI.settings.PRECISION_VERTEX = PIXI.PRECISION.HIGH;
-		} else {
-			PIXI.settings.PRECISION_FRAGMENT = PIXI.PRECISION.MEDIUM;
-			PIXI.settings.PRECISION_VERTEX = PIXI.PRECISION.MEDIUM;
 		}
 		SceneManager.run(Scene_Boot);
 	};
