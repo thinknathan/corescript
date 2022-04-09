8be04ce24c55a193fa96a1f4f5581a545b082c6a
Nathan Bolton <nathan@nathanbolton.ca>
2022-04-09 16:08:17 -0700

    Convert plugins to ES7 with Lebab

--- js/plugins/Debug_ReportMemory.js
+++ js/plugins/Debug_ReportMemory.js
@@ -44,101 +44,96 @@
  *
  */
 
-(function(){
-    var parameters = PluginManager.parameters('Debug_ReportMemory');
-    var pixels = toNumber(parameters['Max Pixels In MPix'], 10);
-    var communityPixels = PluginManager.parameters('Community_Basic')['cacheLimit'];
+((() => {
+	const parameters = PluginManager.parameters('Debug_ReportMemory');
+	let pixels = toNumber(parameters['Max Pixels In MPix'], 10);
+	const communityPixels = PluginManager.parameters('Community_Basic')['cacheLimit'];
 
-    if (communityPixels) {
-        pixels = toNumber(communityPixels, 10);
-        console.log('Community_Basic plugin has been installed, so it overwrites the setting value of cacheLimit. ' +
-        'cacheLimit: ' + pixels + 'MPix');
-    }
+	if (communityPixels) {
+		pixels = toNumber(communityPixels, 10);
+		console.log(`Community_Basic plugin has been installed, so it overwrites the setting value of cacheLimit. cacheLimit: ${pixels}MPix`);
+	}
 
-    ImageCache.limit = pixels * 1000 * 1000;
+	ImageCache.limit = pixels * 1000 * 1000;
 
-    var div = document.createElement('div');
-    div.style.backgroundColor = 'AliceBlue';
-    div.style.position = 'fixed';
-    div.style.right = 0;
-    div.style.top = 0;
-    document.body.appendChild(div);
+	const div = document.createElement('div');
+	div.style.backgroundColor = 'AliceBlue';
+	div.style.position = 'fixed';
+	div.style.right = 0;
+	div.style.top = 0;
+	document.body.appendChild(div);
 
-    function toNumber(str, def) {
-        return isNaN(str) ? def : +(str || def);
-    }
+	function toNumber(str, def) {
+		return isNaN(str) ? def : +(str || def);
+	}
 
-    function toMPix(size) {
-        return Math.floor(size / 1000) / 1000 + 'MPix';
-    }
+	function toMPix(size) {
+		return `${Math.floor(size / 1000) / 1000}MPix`;
+	}
 
-    function usedSize(item, bitmaps) {
-        var totalSize = 0;
-        if (item && item.bitmap && !bitmaps.contains(item.bitmap)) {
-            totalSize += item.bitmap.width * item.bitmap.height;
-            bitmaps.push(item.bitmap);
-        }
-        if (item && item.children) {
-            totalSize += item.children.reduce(function(sum, child) {
-                return sum + usedSize(child, bitmaps);
-            }, 0);
-        }
-        return totalSize;
-    }
+	function usedSize(item, bitmaps) {
+		let totalSize = 0;
+		if (item && item.bitmap && !bitmaps.contains(item.bitmap)) {
+			totalSize += item.bitmap.width * item.bitmap.height;
+			bitmaps.push(item.bitmap);
+		}
+		if (item && item.children) {
+			totalSize += item.children.reduce((sum, child) => sum + usedSize(child, bitmaps), 0);
+		}
+		return totalSize;
+	}
 
-    function updateInfo(){
-        var content = 'usedSize: ' + toMPix(usedSize(SceneManager._scene, [])) + '<br>';
-        content += 'cachedSize: ' + toMPix(ImageManager._imageCache._getSize()) + '<br>';
-        content += '(cacheLimit: ' + pixels + 'MPix)<br>';
-        content += 'totalCount: ' + ImageManager._imageCache._countBitmap() + '<br>';
-        content += 'requestCount: ' + ImageManager._imageCache._countRequest() + '<br>';
-        content += 'reservedCount: ' + ImageManager._imageCache._countReserved() + '<br>';
+	function updateInfo() {
+		let content = `usedSize: ${toMPix(usedSize(SceneManager._scene, []))}<br>`;
+		content += `cachedSize: ${toMPix(ImageManager._imageCache._getSize())}<br>`;
+		content += `(cacheLimit: ${pixels}MPix)<br>`;
+		content += `totalCount: ${ImageManager._imageCache._countBitmap()}<br>`;
+		content += `requestCount: ${ImageManager._imageCache._countRequest()}<br>`;
+		content += `reservedCount: ${ImageManager._imageCache._countReserved()}<br>`;
 
-        if (div.innerHTML !== content) div.innerHTML = content;
-        div.style.zIndex = 11;
-    }
+		if (div.innerHTML !== content) div.innerHTML = content;
+		div.style.zIndex = 11;
+	}
 
-    var ImageManager_update = ImageManager.update;
-    ImageManager.update = function(){
-        ImageManager_update.call(this);
-        updateInfo();
-    };
+	const ImageManager_update = ImageManager.update;
+	ImageManager.update = function () {
+		ImageManager_update.call(this);
+		updateInfo();
+	};
 
-    ImageCache.prototype._getSize = function(){
-        var totalSize = 0;
-        var items = this._items;
+	ImageCache.prototype._getSize = function () {
+		let totalSize = 0;
+		const items = this._items;
 
-        Object.keys(items).forEach(function(key){
-            var bitmap = items[key].bitmap;
-            totalSize += bitmap.width * bitmap.height;
-        });
+		Object.keys(items)
+			.forEach(key => {
+				const bitmap = items[key].bitmap;
+				totalSize += bitmap.width * bitmap.height;
+			});
 
-        return totalSize;
-    };
+		return totalSize;
+	};
 
-    ImageCache.prototype._countBitmap = function(){
-        return Object.keys(this._items).length;
-    };
+	ImageCache.prototype._countBitmap = function () {
+		return Object.keys(this._items)
+			.length;
+	};
 
-    ImageCache.prototype._countRequest = function(){
-        var items = this._items;
+	ImageCache.prototype._countRequest = function () {
+		const items = this._items;
 
-        return Object.keys(this._items)
-            .map(function(key){
-                return items[key].bitmap;
-            }).filter(function(bitmap){
-                return bitmap.isRequestOnly();
-            }).length;
-    };
+		return Object.keys(this._items)
+			.map(key => items[key].bitmap)
+			.filter(bitmap => bitmap.isRequestOnly())
+			.length;
+	};
 
-    ImageCache.prototype._countReserved = function(){
-        var items = this._items;
+	ImageCache.prototype._countReserved = function () {
+		const items = this._items;
 
-        return Object.keys(this._items)
-            .map(function(key){
-                return items[key].reservationId;
-            }).filter(function(id){
-                return id;
-            }).length;
-    };
-})();
+		return Object.keys(this._items)
+			.map(key => items[key].reservationId)
+			.filter(id => id)
+			.length;
+	};
+}))();
