b4571b4ecf8888ef46906dd097fe8b4c64a0704f
Nathan Bolton <nathan@nathanbolton.ca>
2022-04-10 10:09:44 -0700

    Add rollup to package library

---
+++ rollup.config.js
@@ -0,0 +1,41 @@
+import { terser } from 'rollup-plugin-terser';
+
+const pluginList = [
+  terser({
+    ecma: 2018,
+    compress: {
+      drop_console: true,
+      passes: 2,
+    },
+  })
+];
+const formatType = 'iife';
+const banner = `/*!
+ * main.js - corescript v1.6.1 (community-1.4)
+ * (c) 2015 KADOKAWA CORPORATION./YOJI OJIMA
+ * Contributions by the RPG Maker MV CoreScript team
+ * https://github.com/thinknathan/corescript/blob/master/CONTRIBUTORS.md
+ *
+ * Licensed under the MIT License.
+ * https://github.com/thinknathan/corescript/blob/master/LICENSE
+ */`;
+
+export default [
+  {
+    input: 'js/main.js',
+    output: [{
+      format: formatType,
+      file: 'dist/main.js',
+      banner: banner,
+    }],
+    plugins: pluginList,
+  },
+  {
+    input: 'js/main.js',
+    output: [{
+      format: formatType,
+      file: 'dist/main.uncompressed.js',
+      banner: banner,
+    }],
+  },
+];
