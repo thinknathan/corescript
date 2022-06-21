ff48c5ebc0fddc266b268d563489c0d684a93687
Nathan Bolton <nathan@nathanbolton.ca>
2022-06-20 22:28:51 -0700

    Update Main_Thread.js
    finish event passing setup

--- www/js/Main_Thread.js
+++ www/js/Main_Thread.js
@@ -21,80 +21,153 @@ class Main_Thread {
 	}
 	static async attachListeners(Render_Thread) {
 		const isSupportPassive = Utils.isSupportPassiveEvent();
+		const throttleThreshold = 33.34;
 
-			document.addEventListener('keydown', (e) => Render_Thread.documentEventFired('keydown', {
-				key: e.key,
-				code: e.code,
-				altKey: e.altKey,
-				charCode: e.charCode,
-				ctrlKey: e.ctrlKey,
-				keyCode: e.keyCode,
-				shiftKey: e.shiftKey,
-			}));
-			document.addEventListener('keyup', (e) => Render_Thread.documentEventFired('keyup', {
-				key: e.key,
-				code: e.code,
-				altKey: e.altKey,
-				charCode: e.charCode,
-				ctrlKey: e.ctrlKey,
-				keyCode: e.keyCode,
-				shiftKey: e.shiftKey,
-			}));
-			document.addEventListener('mousedown', (e) => Render_Thread.documentEventFired('mousedown', {
-				clientX: e.clientX,
-				clientY: e.clientY,
-				button: e.button,
-				layerX: e.layerX,
-				layerY: e.layerY,
-				offsetX: e.offsetX,
-				offsetY: e.offsetY,
-				pageX: e.pageX,
-				pageY: e.pageY,
-				screenX: e.screenX,
-				screenY: e.screenY,
-				timeStamp: e.timeStamp,
-				x: e.x,
-				y: e.y,
-			}));
-			document.addEventListener('touchend', (e) => Render_Thread.documentEventFired('touchend', {
-				changedTouches: JSON.stringify(e.changedTouches),
-				timeStamp: e.timeStamp,
-				touches: JSON.stringify(e.touches),
-			}));
-			document.addEventListener('touchstart', (e) => Render_Thread.documentEventFired('touchstart', {
-				changedTouches: JSON.stringify(e.changedTouches),
-				timeStamp: e.timeStamp,
-				touches: JSON.stringify(e.touches),
-			}), isSupportPassive ? {
-						passive: false
-					} : false);
+		/* Keyboard Events */
+		document.addEventListener('keydown', (e) => Render_Thread.receiveDocumentEvent('keydown', {
+			key: e.key,
+			code: e.code,
+			altKey: e.altKey,
+			charCode: e.charCode,
+			ctrlKey: e.ctrlKey,
+			keyCode: e.keyCode,
+			shiftKey: e.shiftKey,
+		}));
+		document.addEventListener('keyup', (e) => Render_Thread.receiveDocumentEvent('keyup', {
+			key: e.key,
+			code: e.code,
+			altKey: e.altKey,
+			charCode: e.charCode,
+			ctrlKey: e.ctrlKey,
+			keyCode: e.keyCode,
+			shiftKey: e.shiftKey,
+		}));
 
-			// May need 16ms throttle
-			document.addEventListener('touchmove', (e) => Render_Thread.documentEventFired('touchmove', {
-				changedTouches: JSON.stringify(e.changedTouches),
-				timeStamp: e.timeStamp,
-				touches: JSON.stringify(e.touches),
-			}), isSupportPassive ? {
-						passive: false
-					} : false);
-			document.addEventListener('touchcancel', (e) => Render_Thread.documentEventFired('touchcancel', {}));
+		/* Mouse Events */
+		document.addEventListener('mousedown', (e) => Render_Thread.receiveDocumentEvent('mousedown', {
+			clientX: e.clientX,
+			clientY: e.clientY,
+			button: e.button,
+			layerX: e.layerX,
+			layerY: e.layerY,
+			offsetX: e.offsetX,
+			offsetY: e.offsetY,
+			pageX: e.pageX,
+			pageY: e.pageY,
+			screenX: e.screenX,
+			screenY: e.screenY,
+			timeStamp: e.timeStamp,
+			x: e.x,
+			y: e.y,
+		}));
+		document.addEventListener('mouseup', (e) => Render_Thread.receiveDocumentEvent('mouseup', {
+			clientX: e.clientX,
+			clientY: e.clientY,
+			button: e.button,
+			layerX: e.layerX,
+			layerY: e.layerY,
+			offsetX: e.offsetX,
+			offsetY: e.offsetY,
+			pageX: e.pageX,
+			pageY: e.pageY,
+			screenX: e.screenX,
+			screenY: e.screenY,
+			timeStamp: e.timeStamp,
+			x: e.x,
+			y: e.y,
+		}));
+		const mousemoveFunc = (e) => Render_Thread.receiveDocumentEvent('mousemove', {
+			clientX: e.clientX,
+			clientY: e.clientY,
+			button: e.button,
+			layerX: e.layerX,
+			layerY: e.layerY,
+			offsetX: e.offsetX,
+			offsetY: e.offsetY,
+			pageX: e.pageX,
+			pageY: e.pageY,
+			screenX: e.screenX,
+			screenY: e.screenY,
+			timeStamp: e.timeStamp,
+			x: e.x,
+			y: e.y,
+		});
+		const mousemoveThrottled = Utils.getThrottledFunction(mousemoveFunc, throttleThreshold);
+		document.addEventListener('mousemove', (e) => mousemoveThrottled(e));
 
-			// May need 16.67ms throttle
-			// document.addEventListener('mousemove', (e) => console.log(e));
-			// document.addEventListener('mouseup', (e) => console.log(e));
+		const wheelFunc = (e) => Render_Thread.receiveDocumentEvent('wheel', {
+			deltaX: e.deltaX,
+			deltaY: e.deltaY,
+			deltaZ: e.deltaZ,
+		});
+		const wheelThrottled = Utils.getThrottledFunction(wheelFunc, throttleThreshold);
+		document.addEventListener('wheel', (e) => wheelThrottled(e), isSupportPassive ? {
+					passive: false
+				} : false);
 
-			// May need 16.67ms throttle
-			// document.addEventListener('wheel', (e) => console.log(e), isSupportPassive ? {
-			// 			passive: false
-			// 		} : false);
+		/* Touch Events */
+		document.addEventListener('touchend', (e) => Render_Thread.receiveDocumentEvent('touchend', {
+			changedTouches: JSON.stringify(e.changedTouches),
+			timeStamp: e.timeStamp,
+			touches: JSON.stringify(e.touches),
+		}));
+		document.addEventListener('touchstart', (e) => Render_Thread.receiveDocumentEvent('touchstart', {
+			changedTouches: JSON.stringify(e.changedTouches),
+			timeStamp: e.timeStamp,
+			touches: JSON.stringify(e.touches),
+		}), isSupportPassive ? {
+					passive: false
+				} : false);
+		const touchmoveFunc = (e) => Render_Thread.receiveDocumentEvent('touchmove', {
+			changedTouches: JSON.stringify(e.changedTouches),
+			timeStamp: e.timeStamp,
+			touches: JSON.stringify(e.touches),
+		});
+		const touchmoveThrottled = Utils.getThrottledFunction(touchmoveFunc, throttleThreshold);
+		document.addEventListener('touchmove', (e) => touchmoveThrottled(e), isSupportPassive ? {
+					passive: false
+				} : false);
+		document.addEventListener('touchcancel', (e) => Render_Thread.receiveDocumentEvent('touchcancel', {}));
 
-			// document.addEventListener('pointerdown', (e) => console.log(e));
-			// document.addEventListener('visibilitychange', (e) => console.log(e));
+		/* Other Events */
+		document.addEventListener('pointerdown', (e) => Render_Thread.receiveDocumentEvent('pointerdown', {
+			clientX: e.clientX,
+			clientY: e.clientY,
+			button: e.button,
+			layerX: e.layerX,
+			layerY: e.layerY,
+			offsetX: e.offsetX,
+			offsetY: e.offsetY,
+			pageX: e.pageX,
+			pageY: e.pageY,
+			screenX: e.screenX,
+			screenY: e.screenY,
+			timeStamp: e.timeStamp,
+			x: e.x,
+			y: e.y,
+		}));
 
-			// May need 16.67ms throttle
-			// window.addEventListener('resize', (e) => console.log(e));
-			// window.addEventListener('blur', (e) => console.log(e));
-			// window.addEventListener('error', (e) => console.log(e));
+		document.addEventListener('visibilitychange', (e) => Render_Thread.receiveDocumentEvent('visibilitychange', {
+			timeStamp: e.timeStamp,
+		}));
+
+		const resizeFunc = (e) => Render_Thread.receiveWindowEvent('resize', {
+			innerWidth: window.innerWidth,
+            innerHeight: window.innerHeight,
+			timeStamp: e.timeStamp,
+		});
+		const resizeThrottled = Utils.getThrottledFunction(resizeFunc, throttleThreshold);
+		window.addEventListener('resize', (e) => resizeThrottled(e));
+
+		window.addEventListener('blur', (e) => Render_Thread.receiveWindowEvent('blur', {
+			timeStamp: e.timeStamp,
+		}));
+
+		window.addEventListener('error', (e) => Render_Thread.receiveWindowEvent('error', {
+			timeStamp: e.timeStamp,
+			message: e.message,
+			filename: e.filename,
+		}));
 	}
 	static async transferWindowData (Render_Thread) {
 		const windowData = this.getWindowData();
