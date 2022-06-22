bb01940bb7abaa6624cefa3763cfe4acd4ffb8db
Nathan Bolton <nathan@nathanbolton.ca>
2022-06-21 20:06:54 -0700

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
 import { terser } from 'rollup-plugin-terser';
+import urlResolve from 'rollup-plugin-url-resolve';
 
 const pluginList = [
+  urlResolve(),
   terser({
     ecma: 2018,
     compress: {
@@ -10,32 +12,57 @@ const pluginList = [
   })
 ];
 const formatType = 'iife';
-const banner = `/*!
- * main.js - corescript v1.6.1 (community-1.4)
- * (c) 2015 KADOKAWA CORPORATION./YOJI OJIMA
- * Contributions by the RPG Maker MV CoreScript team
- * https://github.com/thinknathan/corescript/blob/master/CONTRIBUTORS.md
- *
- * Licensed under the MIT License.
- * https://github.com/thinknathan/corescript/blob/master/LICENSE
- */`;
 
 export default [
   {
-    input: 'www/js/main.js',
+    input: 'www/js/Data_Thread.js',
     output: [{
       format: formatType,
-      file: 'dist/main.min.js',
-      banner: banner,
+      file: 'dist/Data_Thread.js',
+      banner: `/*!
+      * Data_Thread.js - corescript v1.6.1 (community-1.4)
+      * (c) 2015 KADOKAWA CORPORATION./YOJI OJIMA
+      * Contributions by the RPG Maker MV CoreScript team
+      * https://github.com/thinknathan/corescript/blob/master/CONTRIBUTORS.md
+      *
+      * Licensed under the MIT License.
+      * https://github.com/thinknathan/corescript/blob/master/LICENSE
+      */`,
     }],
     plugins: pluginList,
   },
   {
-    input: 'www/js/main.js',
+    input: 'www/js/Main_Thread.js',
     output: [{
       format: formatType,
-      file: 'dist/main.js',
-      banner: banner,
+      file: 'dist/Main_Thread.js',
+      banner: `/*!
+      * Main_Thread.js - corescript v1.6.1 (community-1.4)
+      * (c) 2015 KADOKAWA CORPORATION./YOJI OJIMA
+      * Contributions by the RPG Maker MV CoreScript team
+      * https://github.com/thinknathan/corescript/blob/master/CONTRIBUTORS.md
+      *
+      * Licensed under the MIT License.
+      * https://github.com/thinknathan/corescript/blob/master/LICENSE
+      */`,
     }],
+    plugins: pluginList,
+  },
+  {
+    input: 'www/js/Render_Thread.js',
+    output: [{
+      format: formatType,
+      file: 'dist/Render_Thread.js',
+      banner: `/*!
+      * Render_Thread.js - corescript v1.6.1 (community-1.4)
+      * (c) 2015 KADOKAWA CORPORATION./YOJI OJIMA
+      * Contributions by the RPG Maker MV CoreScript team
+      * https://github.com/thinknathan/corescript/blob/master/CONTRIBUTORS.md
+      *
+      * Licensed under the MIT License.
+      * https://github.com/thinknathan/corescript/blob/master/LICENSE
+      */`,
+    }],
+    plugins: pluginList,
   },
 ];
