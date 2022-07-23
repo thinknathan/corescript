ade8c182f61c35396b49d29880843ce1bd362a50
Nathan Bolton <nathan@nathanbolton.ca>
2022-07-23 10:12:12 -0700

    Update pixi.js v6.4.2 -> v6.5.0

--- www/js/libs/pixi.js
+++ www/js/libs/pixi.js
@@ -1,63 +1,63 @@
-export * from 'https://cdn.skypack.dev/pin/@pixi/constants@v6.4.2-xcR2AGIM9jAaa4yYazOt/mode=imports/optimized/@pixi/constants.js';
-export * from 'https://cdn.skypack.dev/pin/@pixi/math@v6.4.2-4xiwKZ3R8IvAkJwyVvZe/mode=imports/optimized/@pixi/math.js';
-export * from 'https://cdn.skypack.dev/pin/@pixi/runner@v6.4.2-eDXYgSMd8xGBFGsc7qZp/mode=imports/optimized/@pixi/runner.js';
-export * from 'https://cdn.skypack.dev/pin/@pixi/settings@v6.4.2-4QQjz92YWLrDV9XmCLX8/mode=imports/optimized/@pixi/settings.js';
-export * from 'https://cdn.skypack.dev/pin/@pixi/ticker@v6.4.2-UE7mRiQGzpsdf5DGVZXJ/mode=imports/optimized/@pixi/ticker.js';
-import * as utils from 'https://cdn.skypack.dev/pin/@pixi/utils@v6.4.2-oyuBZMchwCwa9CRmcvpZ/mode=imports/optimized/@pixi/utils.js';
+export * from 'https://cdn.skypack.dev/pin/@pixi/constants@v6.5.0-xcR2AGIM9jAaa4yYazOt/mode=imports/optimized/@pixi/constants.js';
+export * from 'https://cdn.skypack.dev/pin/@pixi/math@v6.5.0-4xiwKZ3R8IvAkJwyVvZe/mode=imports/optimized/@pixi/math.js';
+export * from 'https://cdn.skypack.dev/pin/@pixi/runner@v6.5.0-eDXYgSMd8xGBFGsc7qZp/mode=imports/optimized/@pixi/runner.js';
+export * from 'https://cdn.skypack.dev/pin/@pixi/settings@v6.5.0-4QQjz92YWLrDV9XmCLX8/mode=imports/optimized/@pixi/settings.js';
+export * from 'https://cdn.skypack.dev/pin/@pixi/ticker@v6.5.0-UE7mRiQGzpsdf5DGVZXJ/mode=imports/optimized/@pixi/ticker.js';
+import * as utils from 'https://cdn.skypack.dev/pin/@pixi/utils@v6.5.0-oyuBZMchwCwa9CRmcvpZ/mode=imports/optimized/@pixi/utils.js';
 export { utils };
-export * from 'https://cdn.skypack.dev/pin/@pixi/display@v6.4.2-ETW2GN0Ls8YE8EpVnmkV/mode=imports/optimized/@pixi/display.js';
-export * from 'https://cdn.skypack.dev/pin/@pixi/core@v6.4.2-lcBTl2aBaz14w51lE2up/mode=imports/optimized/@pixi/core.js';
-import 'https://cdn.skypack.dev/pin/@pixi/canvas-display@v6.4.2-scI6HNSoI7e1q8ViaitW/mode=imports/optimized/@pixi/canvas-display.js';
-export * from 'https://cdn.skypack.dev/pin/@pixi/extract@v6.4.2-7mUZqafTrGGzyDXT1AAv/mode=imports/optimized/@pixi/extract.js';
-export * from 'https://cdn.skypack.dev/pin/@pixi/mesh@v6.4.2-9boCdVrpzj0UtXbrRvHj/mode=imports/optimized/@pixi/mesh.js';
-export * from 'https://cdn.skypack.dev/pin/@pixi/sprite@v6.4.2-eUKJYWgBp3yGJnuXDrH0/mode=imports/optimized/@pixi/sprite.js';
-export * from 'https://cdn.skypack.dev/pin/@pixi/canvas-renderer@v6.4.2-sWzzOxgn1tGBWB08VWQE/mode=imports/optimized/@pixi/canvas-renderer.js';
-export * from 'https://cdn.skypack.dev/pin/@pixi/canvas-extract@v6.4.2-4qhsX4AwF3ljCNGhVWYz/mode=imports/optimized/@pixi/canvas-extract.js';
-export * from 'https://cdn.skypack.dev/pin/@pixi/app@v6.4.2-mDml1ZEiWNC1Y4mr3CSr/mode=imports/optimized/@pixi/app.js';
-export * from 'https://cdn.skypack.dev/pin/@pixi/graphics@v6.4.2-YO0nFPF4nt7tAe6dEXWM/mode=imports/optimized/@pixi/graphics.js';
-export * from 'https://cdn.skypack.dev/pin/@pixi/mesh-extras@v6.4.2-vmni2X2NE1JRHiZX66qs/mode=imports/optimized/@pixi/mesh-extras.js';
-export * from 'https://cdn.skypack.dev/pin/@pixi/loaders@v6.4.2-kSH5X3gU5sNTsHvj9rLG/mode=imports/optimized/@pixi/loaders.js';
-import 'https://cdn.skypack.dev/pin/@pixi/mixin-cache-as-bitmap@v6.4.2-QANCt3kYjAbICTljCEFE/mode=imports/optimized/@pixi/mixin-cache-as-bitmap.js';
-export * from 'https://cdn.skypack.dev/pin/@pixi/sprite-tiling@v6.4.2-JeJLVzDVYDtXCHI3rF5H/mode=imports/optimized/@pixi/sprite-tiling.js';
-export * from 'https://cdn.skypack.dev/pin/@pixi/canvas-sprite@v6.4.2-Dc4l6KhaPzPQ6UGyvmvx/mode=imports/optimized/@pixi/canvas-sprite.js';
-export * from 'https://cdn.skypack.dev/pin/@pixi/text-bitmap@v6.4.2-UWyaExML4qqfSoM2iOFD/mode=imports/optimized/@pixi/text-bitmap.js';
-export * from 'https://cdn.skypack.dev/pin/@pixi/text@v6.4.2-nLnBVYtagxSTAcn8jNZL/mode=imports/optimized/@pixi/text.js';
-export * from 'https://cdn.skypack.dev/pin/@pixi/canvas-graphics@v6.4.2-bO187phCgxR8brcLwOIL/mode=imports/optimized/@pixi/canvas-graphics.js';
-export * from 'https://cdn.skypack.dev/pin/@pixi/canvas-mesh@v6.4.2-BTGLCx1MJNmChEcYb5l0/mode=imports/optimized/@pixi/canvas-mesh.js';
-import 'https://cdn.skypack.dev/pin/@pixi/canvas-sprite-tiling@v6.4.2-qQp70yjjBZyMfhMpZq66/mode=imports/optimized/@pixi/canvas-sprite-tiling.js';
+export * from 'https://cdn.skypack.dev/pin/@pixi/display@v6.5.0-ETW2GN0Ls8YE8EpVnmkV/mode=imports/optimized/@pixi/display.js';
+export * from 'https://cdn.skypack.dev/pin/@pixi/core@v6.5.0-lcBTl2aBaz14w51lE2up/mode=imports/optimized/@pixi/core.js';
+import 'https://cdn.skypack.dev/pin/@pixi/canvas-display@v6.5.0-scI6HNSoI7e1q8ViaitW/mode=imports/optimized/@pixi/canvas-display.js';
+export * from 'https://cdn.skypack.dev/pin/@pixi/extract@v6.5.0-7mUZqafTrGGzyDXT1AAv/mode=imports/optimized/@pixi/extract.js';
+export * from 'https://cdn.skypack.dev/pin/@pixi/mesh@v6.5.0-9boCdVrpzj0UtXbrRvHj/mode=imports/optimized/@pixi/mesh.js';
+export * from 'https://cdn.skypack.dev/pin/@pixi/sprite@v6.5.0-eUKJYWgBp3yGJnuXDrH0/mode=imports/optimized/@pixi/sprite.js';
+export * from 'https://cdn.skypack.dev/pin/@pixi/canvas-renderer@v6.5.0-sWzzOxgn1tGBWB08VWQE/mode=imports/optimized/@pixi/canvas-renderer.js';
+export * from 'https://cdn.skypack.dev/pin/@pixi/canvas-extract@v6.5.0-4qhsX4AwF3ljCNGhVWYz/mode=imports/optimized/@pixi/canvas-extract.js';
+export * from 'https://cdn.skypack.dev/pin/@pixi/app@v6.5.0-mDml1ZEiWNC1Y4mr3CSr/mode=imports/optimized/@pixi/app.js';
+export * from 'https://cdn.skypack.dev/pin/@pixi/graphics@v6.5.0-YO0nFPF4nt7tAe6dEXWM/mode=imports/optimized/@pixi/graphics.js';
+export * from 'https://cdn.skypack.dev/pin/@pixi/mesh-extras@v6.5.0-vmni2X2NE1JRHiZX66qs/mode=imports/optimized/@pixi/mesh-extras.js';
+export * from 'https://cdn.skypack.dev/pin/@pixi/loaders@v6.5.0-kSH5X3gU5sNTsHvj9rLG/mode=imports/optimized/@pixi/loaders.js';
+import 'https://cdn.skypack.dev/pin/@pixi/mixin-cache-as-bitmap@v6.5.0-QANCt3kYjAbICTljCEFE/mode=imports/optimized/@pixi/mixin-cache-as-bitmap.js';
+export * from 'https://cdn.skypack.dev/pin/@pixi/sprite-tiling@v6.5.0-JeJLVzDVYDtXCHI3rF5H/mode=imports/optimized/@pixi/sprite-tiling.js';
+export * from 'https://cdn.skypack.dev/pin/@pixi/canvas-sprite@v6.5.0-Dc4l6KhaPzPQ6UGyvmvx/mode=imports/optimized/@pixi/canvas-sprite.js';
+export * from 'https://cdn.skypack.dev/pin/@pixi/text-bitmap@v6.5.0-UWyaExML4qqfSoM2iOFD/mode=imports/optimized/@pixi/text-bitmap.js';
+export * from 'https://cdn.skypack.dev/pin/@pixi/text@v6.5.0-nLnBVYtagxSTAcn8jNZL/mode=imports/optimized/@pixi/text.js';
+export * from 'https://cdn.skypack.dev/pin/@pixi/canvas-graphics@v6.5.0-bO187phCgxR8brcLwOIL/mode=imports/optimized/@pixi/canvas-graphics.js';
+export * from 'https://cdn.skypack.dev/pin/@pixi/canvas-mesh@v6.5.0-BTGLCx1MJNmChEcYb5l0/mode=imports/optimized/@pixi/canvas-mesh.js';
+import 'https://cdn.skypack.dev/pin/@pixi/canvas-sprite-tiling@v6.5.0-qQp70yjjBZyMfhMpZq66/mode=imports/optimized/@pixi/canvas-sprite-tiling.js';
 
 // Renderer plugins
-import { Renderer } from 'https://cdn.skypack.dev/pin/@pixi/core@v6.4.2-lcBTl2aBaz14w51lE2up/mode=imports/optimized/@pixi/core.js';
-import { BatchRenderer } from 'https://cdn.skypack.dev/pin/@pixi/core@v6.4.2-lcBTl2aBaz14w51lE2up/mode=imports/optimized/@pixi/core.js';
+import { Renderer } from 'https://cdn.skypack.dev/pin/@pixi/core@v6.5.0-lcBTl2aBaz14w51lE2up/mode=imports/optimized/@pixi/core.js';
+import { BatchRenderer } from 'https://cdn.skypack.dev/pin/@pixi/core@v6.5.0-lcBTl2aBaz14w51lE2up/mode=imports/optimized/@pixi/core.js';
 Renderer.registerPlugin('batch', BatchRenderer);
-import { Extract } from 'https://cdn.skypack.dev/pin/@pixi/extract@v6.4.2-7mUZqafTrGGzyDXT1AAv/mode=imports/optimized/@pixi/extract.js';
+import { Extract } from 'https://cdn.skypack.dev/pin/@pixi/extract@v6.5.0-7mUZqafTrGGzyDXT1AAv/mode=imports/optimized/@pixi/extract.js';
 Renderer.registerPlugin('extract', Extract);
-import { TilingSpriteRenderer } from 'https://cdn.skypack.dev/pin/@pixi/sprite-tiling@v6.4.2-JeJLVzDVYDtXCHI3rF5H/mode=imports/optimized/@pixi/sprite-tiling.js';
+import { TilingSpriteRenderer } from 'https://cdn.skypack.dev/pin/@pixi/sprite-tiling@v6.5.0-JeJLVzDVYDtXCHI3rF5H/mode=imports/optimized/@pixi/sprite-tiling.js';
 Renderer.registerPlugin('tilingSprite', TilingSpriteRenderer);
 import { TileRenderer } from "https://cdn.skypack.dev/pin/@pixi/tilemap@v3.2.2-2dMlZoW3YNxu69J1N1DJ/mode=imports/optimized/@pixi/tilemap.js";
 Renderer.registerPlugin('tilemap', TileRenderer);
 
 // CanvasRenderer plugins
-import { CanvasRenderer } from 'https://cdn.skypack.dev/pin/@pixi/canvas-renderer@v6.4.2-sWzzOxgn1tGBWB08VWQE/mode=imports/optimized/@pixi/canvas-renderer.js';
-import { CanvasExtract } from 'https://cdn.skypack.dev/pin/@pixi/canvas-extract@v6.4.2-4qhsX4AwF3ljCNGhVWYz/mode=imports/optimized/@pixi/canvas-extract.js';
+import { CanvasRenderer } from 'https://cdn.skypack.dev/pin/@pixi/canvas-renderer@v6.5.0-sWzzOxgn1tGBWB08VWQE/mode=imports/optimized/@pixi/canvas-renderer.js';
+import { CanvasExtract } from 'https://cdn.skypack.dev/pin/@pixi/canvas-extract@v6.5.0-4qhsX4AwF3ljCNGhVWYz/mode=imports/optimized/@pixi/canvas-extract.js';
 CanvasRenderer.registerPlugin('extract', CanvasExtract);
-import { CanvasGraphicsRenderer } from 'https://cdn.skypack.dev/pin/@pixi/canvas-graphics@v6.4.2-bO187phCgxR8brcLwOIL/mode=imports/optimized/@pixi/canvas-graphics.js';
+import { CanvasGraphicsRenderer } from 'https://cdn.skypack.dev/pin/@pixi/canvas-graphics@v6.5.0-bO187phCgxR8brcLwOIL/mode=imports/optimized/@pixi/canvas-graphics.js';
 CanvasRenderer.registerPlugin('graphics', CanvasGraphicsRenderer);
-import { CanvasMeshRenderer } from 'https://cdn.skypack.dev/pin/@pixi/canvas-mesh@v6.4.2-BTGLCx1MJNmChEcYb5l0/mode=imports/optimized/@pixi/canvas-mesh.js';
+import { CanvasMeshRenderer } from 'https://cdn.skypack.dev/pin/@pixi/canvas-mesh@v6.5.0-BTGLCx1MJNmChEcYb5l0/mode=imports/optimized/@pixi/canvas-mesh.js';
 CanvasRenderer.registerPlugin('mesh', CanvasMeshRenderer);
-import { CanvasSpriteRenderer } from 'https://cdn.skypack.dev/pin/@pixi/canvas-sprite@v6.4.2-Dc4l6KhaPzPQ6UGyvmvx/mode=imports/optimized/@pixi/canvas-sprite.js';
+import { CanvasSpriteRenderer } from 'https://cdn.skypack.dev/pin/@pixi/canvas-sprite@v6.5.0-Dc4l6KhaPzPQ6UGyvmvx/mode=imports/optimized/@pixi/canvas-sprite.js';
 CanvasRenderer.registerPlugin('sprite', CanvasSpriteRenderer);
 import { CanvasTileRenderer } from "https://cdn.skypack.dev/pin/@pixi/tilemap@v3.2.2-2dMlZoW3YNxu69J1N1DJ/mode=imports/optimized/@pixi/tilemap.js";
 CanvasRenderer.registerPlugin('tilemap', CanvasTileRenderer);
 
 // Application plugins
-import { Application } from 'https://cdn.skypack.dev/pin/@pixi/app@v6.4.2-mDml1ZEiWNC1Y4mr3CSr/mode=imports/optimized/@pixi/app.js';
-import { TickerPlugin } from 'https://cdn.skypack.dev/pin/@pixi/ticker@v6.4.2-UE7mRiQGzpsdf5DGVZXJ/mode=imports/optimized/@pixi/ticker.js';
+import { Application } from 'https://cdn.skypack.dev/pin/@pixi/app@v6.5.0-mDml1ZEiWNC1Y4mr3CSr/mode=imports/optimized/@pixi/app.js';
+import { TickerPlugin } from 'https://cdn.skypack.dev/pin/@pixi/ticker@v6.5.0-UE7mRiQGzpsdf5DGVZXJ/mode=imports/optimized/@pixi/ticker.js';
 Application.registerPlugin(TickerPlugin);
 
 // Filters
-import { AlphaFilter } from 'https://cdn.skypack.dev/pin/@pixi/filter-alpha@v6.4.2-0Ds7SgnqICLo5ay3isBL/mode=imports/optimized/@pixi/filter-alpha.js';
-import { ColorMatrixFilter } from 'https://cdn.skypack.dev/pin/@pixi/filter-color-matrix@v6.4.2-0HAZfzvZjcIk87Oy6014/mode=imports/optimized/@pixi/filter-color-matrix.js';
+import { AlphaFilter } from 'https://cdn.skypack.dev/pin/@pixi/filter-alpha@v6.5.0-0Ds7SgnqICLo5ay3isBL/mode=imports/optimized/@pixi/filter-alpha.js';
+import { ColorMatrixFilter } from 'https://cdn.skypack.dev/pin/@pixi/filter-color-matrix@v6.5.0-0HAZfzvZjcIk87Oy6014/mode=imports/optimized/@pixi/filter-color-matrix.js';
 // Needed for every game
 import { PixelateFilter } from 'https://cdn.skypack.dev/pin/@pixi/filter-pixelate@v4.1.3-Mg8FBKUStuMLRZcLkLwP/mode=imports/optimized/@pixi/filter-pixelate.js';
 import { CRTFilter } from 'https://cdn.skypack.dev/pin/@pixi/filter-crt@v4.1.6-8coewTa6TZXKtWcumjrc/mode=imports/optimized/@pixi/filter-crt.js';
