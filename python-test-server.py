c6636c09163870d8a8ca420650e218f4d640435e
Nathan Bolton <nathan@nathanbolton.ca>
2022-09-10 20:37:49 -0700

    Update test server

---
+++ python-test-server.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from http.server import HTTPServer, SimpleHTTPRequestHandler, test
+import sys
+import os
+
+class CORSRequestHandler(SimpleHTTPRequestHandler):
+	def end_headers (self):
+		self.send_header('Cross-Origin-Opener-Policy', 'same-origin')
+		self.send_header('Cross-Origin-Embedder-Policy', 'require-corp')
+		self.send_header('Referrer-Policy', 'no-referrer-when-downgrade')
+		self.send_header('X-Content-Type-Options', 'nosniff')
+		self.send_header('Content-Security-Policy', "frame-ancestors 'self'")
+		SimpleHTTPRequestHandler.end_headers(self)
+
+os.chdir('www')
+
+if __name__ == '__main__':
+	test(CORSRequestHandler, HTTPServer, port=int(sys.argv[1]) if len(sys.argv) > 1 else 8000)
--- run-python-test-server.ps1
+++ run-python-test-server.ps1
@@ -1,3 +1,2 @@
 start-process -FilePath 'C:\Program Files (x86)\BraveSoftware\Brave-Browser\Application\brave.exe' -ArgumentList 'http://localhost:8000?test'
-cd www
-python -m http.server
+python -i python-test-server.py
