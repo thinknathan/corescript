c410b0124c3d85874ebe64e188a281babbac077f
Nathan Bolton <nathan@nathanbolton.ca>
2022-09-14 21:05:31 -0700

    update test server

--- test-server-py-win.ps1
+++ test-server-py-win.ps1
@@ -1,2 +1,2 @@
 start-process -FilePath 'C:\Program Files (x86)\BraveSoftware\Brave-Browser\Application\brave.exe' -ArgumentList 'http://localhost:8000?test'
-python -i python-test-server.py
+python -i test-server-py.py
--- test-server-py.py
+++ test-server-py.py
@@ -3,7 +3,7 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler, test
 import sys
 import os
 
-class CORSRequestHandler(SimpleHTTPRequestHandler):
+class HeaderRequestHandler(SimpleHTTPRequestHandler):
 	def end_headers (self):
 		self.send_header('Cross-Origin-Opener-Policy', 'same-origin')
 		self.send_header('Cross-Origin-Embedder-Policy', 'require-corp')
@@ -15,4 +15,4 @@ class CORSRequestHandler(SimpleHTTPRequestHandler):
 os.chdir('www')
 
 if __name__ == '__main__':
-	test(CORSRequestHandler, HTTPServer, port=int(sys.argv[1]) if len(sys.argv) > 1 else 8000)
+	test(HeaderRequestHandler, HTTPServer, port=int(sys.argv[1]) if len(sys.argv) > 1 else 8000)
