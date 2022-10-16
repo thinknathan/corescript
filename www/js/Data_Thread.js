00aca7fe3565323a79ffa64f64a5892c288e47d0
Nathan Bolton <nathan@nathanbolton.ca>
2022-10-16 16:12:43 -0700

    Update Data_Thread.js

--- www/js/Data_Thread.js
+++ www/js/Data_Thread.js
@@ -1,6 +1,5 @@
 'use strict';
 
-import * as Comlink from 'https://cdn.skypack.dev/pin/comlink@v4.3.1-ebLSsXPUzhGrZgtPT5jX/mode=imports/optimized/comlink.js';
 import StorageManager from './rpg_managers/StorageManager.js';
 
 class Data_Thread {
