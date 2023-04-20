a8ca0d5441502cc488e5bfb4047ad9e4ae7aa991
Nathan Bolton <github@thinknathan.com>
2023-04-19 22:44:32 -0700

    add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,39 @@
+module.exports = {
+	root: true,
+	extends: ['eslint:recommended', 'plugin:import/warnings', 'prettier'],
+	globals: {
+		PIXI: true,
+	},
+	env: {
+		node: true,
+		es6: true,
+		amd: true,
+		browser: true,
+	},
+	parserOptions: {
+		ecmaVersion: 2018,
+		sourceType: 'module',
+	},
+	plugins: ['import', 'prettier'],
+	settings: {
+		'import/core-modules': [],
+		'import/ignore': [
+			'node_modules',
+			'\\.(coffee|scss|css|less|hbs|svg|json)$',
+		],
+	},
+	rules: {
+		'prettier/prettier': 'error',
+		'no-console': 0,
+		'comma-dangle': [
+			'error',
+			{
+				arrays: 'always-multiline',
+				objects: 'always-multiline',
+				imports: 'always-multiline',
+				exports: 'always-multiline',
+				functions: 'ignore',
+			},
+		],
+	},
+};
